7341c745e8c9e21b7f8c35e4b24cda376b4dca58
Anixxi <79166272+Anixxi@users.noreply.github.com>
2021-09-18 19:09:29 +0200

    Add files via upload

---
+++ ProjetAVV/src/Objet.java
@@ -0,0 +1,7 @@
+public class Objet {
+
+    String nom;
+    double Vi;
+    double Pi;
+
+}
---
+++ ProjetAVV/src/SacADos.java
@@ -0,0 +1,30 @@
+public class SacADos {
+    int vi, pi;
+    Objet[] objet;
+
+    //objet = new Objet[9];
+    objet[0]= new Objet("Lampe", 2.0, 30.0);
+
+
+    public SacADos() {
+    }
+
+    public SacADos(String chemin, float poids-maximal) {
+        return
+    }
+
+    @Override
+    public String toString() {
+        return "$>resoudre-sac-ados";
+    }
+
+    private int[] tri(){
+
+    }
+
+    public void resoudreGloutonne(){
+        int rapport = vi/pi;
+
+
+    }
+}
