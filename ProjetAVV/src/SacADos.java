5930282761a74d0c6521930c2aada37bcf3e181c
Anixxi <79166272+Anixxi@users.noreply.github.com>
2021-09-20 20:36:24 +0200

    Update SacADos.java

--- ProjetAVV/src/SacADos.java
+++ ProjetAVV/src/SacADos.java
@@ -1,16 +1,18 @@
 public class SacADos {
-    int vi, pi;
     Objet[] objet;
+    final int poidsMax = 30;
 
     //objet = new Objet[9];
     objet[0]= new Objet("Lampe", 2.0, 30.0);
 
 
     public SacADos() {
+
+
     }
 
-    public SacADos(String chemin, float poids-maximal) {
-        return
+    public SacADos(String chemin, float poidsMax) {
+
     }
 
     @Override
@@ -18,13 +20,27 @@ public class SacADos {
         return "$>resoudre-sac-ados";
     }
 
-    private int[] tri(){
+    static int rechDicho2(/*const*/Objet[] t, int n, int x){ boolean trouve = false;
+        int g = 0;
+        int h = n-1;
+        int m = -1;
+        while (g <= h && !trouve){
+            m = (g+h)/2;
+            if (t[m] == x){ trouve = true;
+            }
+            else if (t[m] < x){
+                g = m+1; }
+            else{
+                h = m-1;
+            } }
+        return (trouve ? m : -1);
+    }
 
+    private double rapport(Objet objet){
+        return objet.Vi/objet.Pi;
     }
 
     public void resoudreGloutonne(){
-        int rapport = vi/pi;
-
 
     }
 }
